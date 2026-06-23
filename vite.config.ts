9886aff010a5cfd7db88d2604fa9af53773fa201
SaratBasava-techie <scs305163@gmail.com>
2026-06-23 18:45:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  vite: {
+    base: "/federal-bank-workflow/",
+  },
+
   tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
+    // Redirect TanStack Start's bundled server entry to src/server.ts
+    // / nitro/vite builds from
     server: { entry: "server" },
   },
 });
